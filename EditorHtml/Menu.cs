2fe5043db4c047107764a9e8ea2eda0c61447782
Marco <51382551+maurelio631@users.noreply.github.com>
2023-01-25 13:45:39 +0000

    Iniciando a estutura do projeto e iniciando personalizações ao console

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace EditorHtml
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+        }
+
+
+    }
+}
